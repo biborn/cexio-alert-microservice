58c019a1b77a49025739289647dca3fd2abcd583
Rashid Razak <rashid@cjpengineering.com>
2017-08-16 02:00:35 +0800

    Added sending notification function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var https = require('https');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -22,7 +23,44 @@ app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('This is RESTFul API for Tortoise Project');
+    sendNotification(message);
 });
+
+var sendNotification = function(data) {
+    var headers = {
+      "Content-Type": "application/json; charset=utf-8",
+      "Authorization": "Basic ZjExZGRjYmQtOWIwOC00MTE2LWFmNDQtNGI4MGExZTUwYWJm"
+    };
+    
+    var options = {
+      host: "onesignal.com",
+      port: 443,
+      path: "/api/v1/notifications",
+      method: "POST",
+      headers: headers
+    };
+    
+    var req = https.request(options, function(res) {  
+      res.on('data', function(data) {
+        console.log("Response:");
+        console.log(JSON.parse(data));
+      });
+    });
+    
+    req.on('error', function(e) {
+      console.log("ERROR:");
+      console.log(e);
+    });
+    
+    req.write(JSON.stringify(data));
+    req.end();
+};
+  
+var message = { 
+    app_id: "c9ab32dd-08c6-463e-98f9-4d61f6cd96e2",
+    contents: {"en": "English Message"},
+    included_segments: ["All"]
+};
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
