41e9f113e6bf815e7b7dc2a35a5b8ac79c405735
Rashid Razak <rashid@cjpengineering.com>
2017-08-16 03:50:36 +0800

    Fix setInterval

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 
 
-
-
 /**
  * Root Endpoint
  */
@@ -24,16 +22,18 @@ app.use(bodyParser.json());
 app.get('/', function (req, res) {
     res.send('This is RESTFul API for Tortoise Project');
 
-    refreshIntervalId = setInterval(function() {
+    refreshIntervalId = setInterval(function () {
 
-        getPrice().then(function(data) {
-            console.log('DATA:', data);
+        getPrice().map(function(result) {
+
+
+            console.log('RESULT:', result);
             var now = new Date();
             var dateString = now.toString();
             var message = {
                 app_id: "c9ab32dd-08c6-463e-98f9-4d61f6cd96e2",
                 headings: {"en": "CEX.IO Alert"},
-                contents: {"en": dateString},
+                contents: {"en": result.toString()},
                 included_segments: ["All"]
             };
 
@@ -50,67 +50,42 @@ app.get('/stop', function (req, res) {
     res.send('Stopping service');
 });
 
-var getPrice = function() {
+var getPrice = function () {
+    return https.get('https://cex.io/api/last_price/BTC/USD').map(function(data) {
+        result = data.json();
+    });
+};
+
+var sendNotification = function (data) {
     var headers = {
-        "Content-Type": "text/json"
+        "Content-Type": "application/json; charset=utf-8",
+        "Authorization": "Basic ZjExZGRjYmQtOWIwOC00MTE2LWFmNDQtNGI4MGExZTUwYWJm"
     };
 
     var options = {
-        host: "cex.io",
+        host: "onesignal.com",
         port: 443,
-        path: "/api/last_price/BTC/USD",
-        method: "GET",
+        path: "/api/v1/notifications",
+        method: "POST",
         headers: headers
     };
 
-    var req = https.request(options, function(res) {
-        res.on('data', function(data) {
+    var req = https.request(options, function (res) {
+        res.on('data', function (data) {
             console.log("Response:");
             console.log(JSON.parse(data));
-            price = data.lprice;
         });
     });
 
-    req.on('error', function(e) {
+    req.on('error', function (e) {
         console.log("ERROR:");
         console.log(e);
     });
 
     req.write(JSON.stringify(data));
     req.end();
-    return price;
 };
 
-var sendNotification = function(data) {
-    var headers = {
-      "Content-Type": "application/json; charset=utf-8",
-      "Authorization": "Basic ZjExZGRjYmQtOWIwOC00MTE2LWFmNDQtNGI4MGExZTUwYWJm"
-    };
-    
-    var options = {
-      host: "onesignal.com",
-      port: 443,
-      path: "/api/v1/notifications",
-      method: "POST",
-      headers: headers
-    };
-    
-    var req = https.request(options, function(res) {  
-      res.on('data', function(data) {
-        console.log("Response:");
-        console.log(JSON.parse(data));
-      });
-    });
-    
-    req.on('error', function(e) {
-      console.log("ERROR:");
-      console.log(e);
-    });
-    
-    req.write(JSON.stringify(data));
-    req.end();
-};
-
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
