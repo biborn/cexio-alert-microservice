ce663eb0b35ca08a3422d98d861f51edfd878f08
Rashid Razak <rashid@cjpengineering.com>
2017-08-16 18:33:49 +0800

    Done for single user.

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.disable('etag');
 
 // Middleware. Use body-parser
 app.use(bodyParser.json());
@@ -19,59 +20,80 @@ app.use(bodyParser.json());
  * Root Endpoint
  */
 
-app.get('/:_setprice', function (req, res) {
+var isRunning = false;
+var refreshIntervalId;
+var limit;
+
+function getLimit() {
+    return limit;
+}
+
+function setLimit(input) {
+    limit = Number(input);
+}
+
+app.get('/', function(req, res) {
+    res.send('LOL');
+});
+
+app.get('/setlimit/:_setprice', function (req, res) {
     res.send('This is RESTFul API for Tortoise Project');
-    setprice = req.params._setprice;
-    /*if (refreshIntervalId) {
-        clearInterval(refreshIntervalId);
-    }*/
+    var setprice = req.params._setprice;
 
-    refreshIntervalId = setInterval(function () {
-        pingHeroku();
-        getPrice(function(result) {
-            console.log('RESULT:', result);
+    setLimit(setprice);
 
-            var treshold = JSON.parse(result);
-            console.log('TRESHOLD:', treshold);
-
-            if (setprice > Number(treshold.lprice)) {
-                console.log('SETPRICE IS LOWER:');
-                console.log('PRICE FROM CEX.IO:', treshold.lprice);
-
-                var now = new Date();
-                var dateString = now.toLocaleString('en-MY', { hour: 'numeric',minute:'numeric', hour12: true, timeZone: 'Asia/Kuala_Lumpur' });
-                var messageString = dateString + '  Bitcoin price is low: USD' + treshold.lprice;
-                var message = {
-                    app_id: "c9ab32dd-08c6-463e-98f9-4d61f6cd96e2",
-                    headings: {"en": "CEX.IO Alert"},
-                    contents: {"en": messageString},
-                    included_segments: ["All"]
-                };
-
-                sendNotification(message);
-            } else {
-                console.log('PRICE IS HIGHER:', treshold.lprice);
-            }
-
-
-
-        });
-
-    }, 600000);
+    if (!isRunning) {
+        refreshIntervalId = setInterval(doStuff, 10000);
+        doStuff();
+        isRunning = true;
+    }
     // sendNotification(message);
 });
 
 app.get('/stop', function (req, res) {
-
     res.send('Stopping service');
-
-    /* later */
     clearInterval(refreshIntervalId);
+    isRunning = false;
 });
 
 app.get('/ping', function (req, res) {
     res.send('Ping Successful!');
 });
+
+function doStuff() {
+    pingHeroku();
+    getPrice(function(result) {
+        console.log('RESULT:', result);
+        processThings(result);
+    });
+}
+
+function processThings(input) {
+    var apiData = JSON.parse(input);
+    console.log('apiData:', apiData);
+
+    var priceLimit = getLimit();
+    var currentPrice = Number(apiData.lprice);
+
+    if (priceLimit > currentPrice) {
+        console.log('SETPRICE IS LOWER:');
+        console.log('PRICE FROM CEX.IO:', currentPrice);
+
+        var now = new Date();
+        var dateString = now.toLocaleString('en-MY', { hour: 'numeric',minute:'numeric', hour12: true, timeZone: 'Asia/Kuala_Lumpur' });
+        var messageString = dateString + '  Bitcoin price is low: USD' + currentPrice;
+        var message = {
+            app_id: "c9ab32dd-08c6-463e-98f9-4d61f6cd96e2",
+            headings: {"en": "CEX.IO Alert"},
+            contents: {"en": messageString},
+            included_segments: ["All"]
+        };
+
+        sendNotification(message);
+    } else {
+        console.log('PRICE IS HIGHER:', currentPrice);
+    }
+}
 
 var pingHeroku = function() {
     var options = {
@@ -142,7 +164,7 @@ var getPrice = function (callback) {
     return body + '... received';
 };
 
-var sendNotification = function (data) {
+function sendNotification(data) {
     var headers = {
         "Content-Type": "application/json; charset=utf-8",
         "Authorization": "Basic ZjExZGRjYmQtOWIwOC00MTE2LWFmNDQtNGI4MGExZTUwYWJm"
